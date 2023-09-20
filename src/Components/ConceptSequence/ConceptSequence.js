42ab0302f8008bb39de47d8915b04662abe7e441
Tabetha Horn <tlhorn88@gmail.com>
2023-09-20 08:09:58 -0500

    added default concept sequence state

--- src/Components/ConceptSequence/ConceptSequence.js
+++ src/Components/ConceptSequence/ConceptSequence.js
@@ -1,12 +1,47 @@
 import './ConceptSequence.css';
-
+import { useState } from 'react';
 
 function ConceptSequence() {
+  const [conceptSequence, changeConceptSequence] = useState([
+    'tuneful singing',
+    'beat',
+    'loud v. soft',
+    'high low chant',
+    'fast slow',
+    'high low melody',
+    'rhythm long short',
+    'qsd',
+    'sm',
+    'Z',
+    'l',
+    '2$',
+    'd',
+    'w',
+    'r',
+    'xxcc',
+    'dP',
+    '4$',
+    'l,',
+    'sxc',
+    's,',
+    'xcd',
+    "d'",
+    'a',
+    'lP',
+    'aqa',
+    'f',
+    'ra',
+    't,',
+    '3$',
+    't',
+    'gc',
+    'ar',
+  ]);
   return (
     <div>
       <h1>CONCEPT SEQUENCE</h1>
     </div>
-  )
+  );
 }
 
 export default ConceptSequence;
--- src/Components/LessonPlanGenerator/LessonPlanGenerator.js
+++ src/Components/LessonPlanGenerator/LessonPlanGenerator.js
@@ -18,7 +18,12 @@ function LessonPlanGenerator() {
           <h3>Spanish</h3>
           <p>Add more</p>
         </div>
-        <div className="contentAndSchedule">
+        <div className="advanced">
+          <h2>Schedule</h2>
+          <p>I see my students for *** minute lessons, *** times a week for *** weeks a semester.</p>
+        
+        </div>
+        <div className="content">
           <h2>Content Options:</h2>
           <h3>Starting points:</h3>
           <h4>Melodic</h4>
@@ -26,15 +31,6 @@ function LessonPlanGenerator() {
           <h3>Ending points:</h3>
           <h4>Melodic</h4>
           <h4>Rhythmic</h4>
-        </div>
-        <div className="advanced">
-          <h2>Advanced (show/hide)</h2>
-          <p>Would you like to modify the template structure?</p>
-          <button>Yes</button>
-          <p>Would you like to modify the concept sequence?</p>
-          <NavLink to="ConceptSequence" onClick={handleConceptSequenceClick}>
-            <button>Yes</button>
-          </NavLink>
         </div>
       </div>
     </div>
