428b5dc8474090f144edf0e24c3a1592f0c6bff6
Tabetha Horn <tlhorn88@gmail.com>
2023-09-27 20:59:56 -0500

    changed grid on lessonPlanGenerator

--- src/Components/LessonPlanGenerator/LessonPlanGenerator.js
+++ src/Components/LessonPlanGenerator/LessonPlanGenerator.js
@@ -9,9 +9,8 @@ function LessonPlanGenerator() {
     (state) => state.conceptSequence.conceptSequence
   );
 
-    const [selectedRhythmicConcept, setSelectedRhythmicConcept] = useState('');
-    const [selectedMelodicConcept, setSelectedMelodicConcept] = useState('');
-
+  const [selectedRhythmicConcept, setSelectedRhythmicConcept] = useState('');
+  const [selectedMelodicConcept, setSelectedMelodicConcept] = useState('');
 
   let kinderSequence = [];
   let rhythmicSequence = [];
@@ -27,18 +26,28 @@ function LessonPlanGenerator() {
     }
   });
 
-
   return (
     <div>
       <h1>LESSON PLAN GENERATOR</h1>
       <div className="lessonPlanGrid">
+
         <div className="demographics">
-          <h2>Grade</h2>
-          <h2>Represented Languages</h2>
+          <h2>Student Demographics</h2>
+          <h3>Grade</h3>
+          <select>
+            <option>0</option>
+            <option>1</option>
+            <option>2</option>
+            <option>3</option>
+            <option>4</option>
+            <option>5</option>
+          </select>
+          <h3>Represented Languages</h3>
           <h3>English</h3>
           <h3>Spanish</h3>
           <p>Add more</p>
         </div>
+
         <div className="advanced">
           <h2>Schedule</h2>
           <p>
@@ -46,46 +55,51 @@ function LessonPlanGenerator() {
             weeks a semester.
           </p>
         </div>
-        <h4>Melodic</h4>
+
         <div className="content">
           <h2>Content Options:</h2>
           <h3>Starting points:</h3>
           <h4>Melodic</h4>
           <select
             value={selectedMelodicConcept}
-            onChange={(e) => setSelectedMelodicConcept(e.target.value)}>
+            onChange={(e) => setSelectedMelodicConcept(e.target.value)}
+          >
             <option value="">Select a Melodic Starting Point</option>
             {melodicSequence.map((concept) => (
               <option key={concept.id} value={concept.concept}>
                 {concept.concept}
               </option>
             ))}
-            </select>
+          </select>
           <h4>Rhythmic</h4>
           <select
             value={selectedRhythmicConcept}
-            onChange={(e) => setSelectedRhythmicConcept(e.target.value)}>
+            onChange={(e) => setSelectedRhythmicConcept(e.target.value)}
+          >
             <option value="">Select a Rhythmic Starting Point</option>
             {rhythmicSequence.map((concept) => (
               <option key={concept.id} value={concept.concept}>
                 {concept.concept}
               </option>
             ))}
-            </select>
+          </select>
           <h3>Ending points:</h3>
           <h4>Melodic</h4>
           <h4>Rhythmic</h4>
         </div>
-        <div className="conceptSequence"></div>
-        <h2>Concept Sequence</h2>
-        <p>(modify)</p>
-        <ul className="conceptList">
-          {conceptSequence.map((concept) => (
-            <li className="concept" key={concept.id}>
-              {concept.concept}
-            </li>
-          ))}
-        </ul>
+
+        <div className="conceptSequence">
+          <h2>Concept Sequence</h2>
+          <p>(modify)</p>
+          <ul className="conceptList">
+            {conceptSequence.map((concept) => (
+              <li className="concept" key={concept.id}>
+                {concept.concept}
+              </li>
+            ))}
+          </ul>
+        </div>
+        
       </div>
     </div>
   );
